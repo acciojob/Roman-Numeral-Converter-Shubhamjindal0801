4ecfe6b1c18e1bfa6e84d5bc574a63e17db32c05
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-25 00:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function convertToRoman(num) {
   	
 	const obj = [
     { value: 1000, symbol: 'M' },
+    { value: 900, symbol: 'CM' },
     { value: 500, symbol: 'D' },
+    { value: 400, symbol: 'CD' },
     { value: 100, symbol: 'C' },
+    { value: 90, symbol: 'XC' },
     { value: 50, symbol: 'L' },
+    { value: 40, symbol: 'XL' },
     { value: 10, symbol: 'X' },
+    { value: 9, symbol: 'IX' },
     { value: 5, symbol: 'V' },
+    { value: 4, symbol: 'IV' },
     { value: 1, symbol: 'I' }
   ];
 
@@ -24,7 +30,7 @@ function convertToRoman(num) {
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
-//console.log(convertToRoman(14))
+console.log(convertToRoman(14))
 
 
 
